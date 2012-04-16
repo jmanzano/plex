954c2c4022b19b6f3a1918398a83216b21ba00d8
Elan Feingold <elan@plexapp.com>
2012-04-16 00:26:14 -1000

    Added a debug version of the release packer.

---
+++ prep-release-debug.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+pushd build/Debug/Plex.app/Contents/Resources/Plex
+rm -rf addons/metadata.*
+rm -rf addons/skin.mediastream/.git
+rm -rf addons/skin.confluence
+cd addons/skin.mediastream
+../../../../../../../../tools/TexturePacker/TexturePacker -input media
+rm media/*
+rm -rf media-lite
+mv Textures.xbt media/
+rm -rf .git
